8356cfa8d5dfda09dbe4f28036418d2528f62c7f
Ravan1190 <71804941+Ravan1190@users.noreply.github.com>
2020-09-24 08:52:23 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -47,7 +47,7 @@ async def rename_message_f(client, message):
     inline_keyboard.append([
         pyrogram.InlineKeyboardButton(
             text="read this?",
-            url="https://t.me/keralagram/698909"
+            url="https://t.me/faster_leech_group"
         )
     ])
     reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
